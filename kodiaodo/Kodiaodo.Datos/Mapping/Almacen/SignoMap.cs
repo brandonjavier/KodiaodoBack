80698ff535e5eb55316e4913a583c8903a601df7
brandonjavier <bjavier_04familia@outlook.com>
2020-12-05 13:02:31 -0500

    commit_espacio_id_activar_desactivar

---
+++ kodiaodo/Kodiaodo.Datos/Mapping/Almacen/SignoMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+using System.Text;
+using Kodiaodo.Entidades.Almacen;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kodiaodo.Datos.Mapping.Almacen
+{
+    public class SignoMap : IEntityTypeConfiguration<Signo>
+    {
+
+    }
+}
